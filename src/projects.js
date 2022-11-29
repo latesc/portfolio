7b5622b74e48bacc34b69db19c87f04803e258fc
Luis Escarrilla <75938195+latesc@users.noreply.github.com>
2022-11-29 08:50:39 +0000

    deleted:    public/Octocat.png 	new file:   public/assets/github.svg 	new file:   public/assets/linkedin.svg 	new file:   public/assets/mail.svg 	new file:   public/assets/twitter.svg 	new file:   public/assets/youtube.svg 	modified:   public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   public/manifest.json 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	new file:   src/projects.js 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	new file:   src/socmeds.js

---
+++ src/projects.js
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Twitter Anniversary Generator",
+    description:
+      "A W.I.P. web app that lets you create a special Twitter link that opens a pop-up with custom parameters. Only works when link is opened in-app, like in DMs or from tweets.",
+    link: "https://twt-anniv.latesc.ml/",
+  },
+];
---
+++ src/socmeds.js
@@ -0,0 +1,30 @@
+export const socmeds = [
+  {
+    id: 1,
+    name: "latesc__",
+    site: "Twitter",
+    link: "https://twitter.com/latesc__",
+    logo: "assets/twitter.svg",
+  },
+  {
+    id: 2,
+    name: "Mr. Quack",
+    site: "YouTube",
+    link: "https://youtube.com/@mrqk00",
+    logo: "assets/youtube.svg",
+  },
+  {
+    id: 3,
+    name: "latesc",
+    site: "Linkedin",
+    link: "https://linkedin.com/in/latesc",
+    logo: "assets/linkedin.svg",
+  },
+  {
+    id: 4,
+    name: "latesc",
+    site: "Github",
+    link: "https://github.com/latesc",
+    logo: "assets/github.svg",
+  },
+];
